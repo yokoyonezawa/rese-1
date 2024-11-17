d7be0756035c1d305a1df0b02666f18a6a161e36
yoko yonezawa <rin_komachi_0112@yahoo.co.jp>
2024-11-18 02:37:59 +0900

    追加実装６直後に戻る

--- src/config/app.php
+++ src/config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
 
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
